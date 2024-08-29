a2c31035c742f2a5bf19b2c2961e01584d15e95f
Ambition Kyndiah <53797801+ambiknds@users.noreply.github.com>
2024-08-29 17:59:33 +0000

    update index header and add resume.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
         </li>
         <li>
           <a
-            href="#"
+            href="./resume.pdf"
             className="bg-gradient-to-r from-cyan-500 to-teal-400 text-white px-4 py-2 rounded-md ml-8"
           >
             Resume
